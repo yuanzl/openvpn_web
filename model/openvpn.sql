54f954f38861bf222ca8224eebd973d54ee0b7ec
lang13002 <18238896@qq.com>
2022-02-27 16:03:53 +0000

    update model/openvpn.sql.

--- model/openvpn.sql
+++ model/openvpn.sql
@@ -1,23 +1,52 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
+/*
+Navicat SQLite Data Transfer
 
+Source Server         : openvpn
+Source Server Version : 30706
+Source Host           : :0
+
+Target Server Type    : SQLite
+Target Server Version : 30706
+File Encoding         : 65001
+
+Date: 2022-02-27 23:09:32
+*/
+
+PRAGMA foreign_keys = OFF;
+
+-- ----------------------------
+-- Table structure for "main"."sqlite_sequence"
+-- ----------------------------
+DROP TABLE "main"."sqlite_sequence";
+CREATE TABLE sqlite_sequence(name,seq);
+
+-- ----------------------------
+-- Records of sqlite_sequence
+-- ----------------------------
+INSERT INTO "main"."sqlite_sequence" VALUES ('t_admin', 1);
+INSERT INTO "main"."sqlite_sequence" VALUES ('t_logs', 1);
+INSERT INTO "main"."sqlite_sequence" VALUES ('t_user', 1);
+
+-- ----------------------------
+-- Table structure for "main"."t_admin"
+-- ----------------------------
+DROP TABLE "main"."t_admin";
 CREATE TABLE "t_admin" (
 "id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 "username"  TEXT NOT NULL,
 "password"  TEXT NOT NULL,
 "token"  TEXT
 );
-INSERT INTO "t_admin" VALUES(1,'admin','e10adc3949ba59abbe56e057f20f883e','');
 
-CREATE TABLE "t_user" (
-"id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-"username"  TEXT NOT NULL,
-"password"  TEXT NOT NULL,
-"active"  INTEGER NOT NULL,
-"expire"  TEXT NOT NULL,
-"firewall"  TEXT
-);
+-- ----------------------------
+-- Records of t_admin
+-- ----------------------------
+INSERT INTO "main"."t_admin" VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'YWRtaW4xNjQ1OTczODE4LjY4MTI5NTY=');
 
+-- ----------------------------
+-- Table structure for "main"."t_logs"
+-- ----------------------------
+DROP TABLE "main"."t_logs";
 CREATE TABLE "t_logs" (
 "id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 "username"  TEXT NOT NULL,
@@ -32,8 +61,59 @@ CREATE TABLE "t_logs" (
 "sent"  INTEGER
 );
 
-DELETE FROM sqlite_sequence;
-INSERT INTO "sqlite_sequence" VALUES('t_admin',1);
-INSERT INTO "sqlite_sequence" VALUES('t_user',1);
-INSERT INTO "sqlite_sequence" VALUES('t_logs',1);
-COMMIT;
+-- ----------------------------
+-- Records of t_logs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for "main"."t_smtp"
+-- ----------------------------
+DROP TABLE "main"."t_smtp";
+CREATE TABLE "t_smtp" (
+"id"  INTEGER NOT NULL,
+"server"  TEXT,
+"ssl"  INTEGER,
+"port"  INTEGER,
+"user"  TEXT,
+"password"  TEXT
+);
+
+-- ----------------------------
+-- Records of t_smtp
+-- ----------------------------
+INSERT INTO "main"."t_smtp" VALUES (1, '', 0, 25, '', '');
+
+-- ----------------------------
+-- Table structure for "main"."t_user"
+-- ----------------------------
+DROP TABLE "main"."t_user";
+CREATE TABLE "t_user" (
+"id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+"username"  TEXT NOT NULL,
+"password"  TEXT NOT NULL,
+"email"  TEXT,
+"active"  INTEGER NOT NULL,
+"expire"  TEXT NOT NULL,
+"firewall"  TEXT,
+"send"  INTEGER
+);
+
+-- ----------------------------
+-- Table structure for "main"."t_warn"
+-- ----------------------------
+DROP TABLE "main"."t_warn";
+CREATE TABLE "t_warn" (
+"id"  INTEGER NOT NULL,
+"status"  INTEGER NOT NULL,
+"send_time"  INTEGER NOT NULL,
+"send_msg"  TEXT,
+"admin_status"  INTEGER NOT NULL,
+"admin_email"  TEXT,
+"admin_msg"  TEXT
+);
+
+-- ----------------------------
+-- Records of t_warn
+-- ----------------------------
+INSERT INTO "main"."t_warn" VALUES (1, 1, 1, '{username}:
+       你的VPN用户在 {time} 到期，如果需要继续使用VPN，请向管理员申请续期。', 1, '', null);
